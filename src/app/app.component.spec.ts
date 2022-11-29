c34059bc07986f360afe46964fa2a156e2c609e5
hennapusa <Henna.Pusa@student.lab.fi>
2022-11-29 10:52:17 +0200

    29.11

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+//RUN Test
+//ng test --include=src/app/app.component.spec.ts
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -26,10 +29,11 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-example-app');
   });
 
+
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-example-app app is running!');
+    expect(compiled.querySelector('.toolbar span')?.textContent).toContain('Dev-ops-kurssin harjoitusprojekti');
   });
 });
